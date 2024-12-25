0a3ae9a6bfcfefcc5f274b31b2ee409699397ee0
edaywalid <becharwalid9@gmail.com>
2024-12-25 03:38:43 +0100

    using CUnit for unit testing instead of assert library

--- practice/c_calculator_tdd/test/calculator_test.c
+++ practice/c_calculator_tdd/test/calculator_test.c
@@ -1,34 +1,46 @@
 #include "../src/calculator.h"
-#include <assert.h>
-#include <stdio.h>
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
 
 void test_add() {
-  assert(add(2, 3) == 5);
-  assert(add(-1, 1) == 0);
-  printf("All addition tests passed.\n");
+  CU_ASSERT(add(2, 3) == 5);
+  CU_ASSERT(add(-1, 1) == 0);
 }
 
 void test_subtract() {
-  assert(sub(5, 3) == 2);
-  assert(sub(0, 5) == -5);
-  printf("All subtraction tests passed.\n");
+  CU_ASSERT(sub(5, 3) == 2);
+  CU_ASSERT(sub(0, 5) == -5);
 }
 
 void test_multiply() {
-  assert(mul(3, 4) == 12);
-  assert(mul(0, 5) == 0);
-  printf("All multiplication tests passed.\n");
+  CU_ASSERT(mul(3, 4) == 12);
+  CU_ASSERT(mul(0, 5) == 0);
 }
 
-void test_divide() {
-  assert(div(6, 3) == 2);
-  printf("All division tests passed.\n");
-}
+void test_divide() { CU_ASSERT(div(6, 3) == 2); }
 
 int main() {
-  test_add();
-  test_subtract();
-  test_multiply();
-  test_divide();
-  return 0;
+  if (CUE_SUCCESS != CU_initialize_registry()) {
+    return CU_get_error();
+  }
+
+  CU_pSuite suite = CU_add_suite("Calculator Tests", 0, 0);
+  if (NULL == suite) {
+    CU_cleanup_registry();
+    return CU_get_error();
+  }
+
+  if ((NULL == CU_add_test(suite, "test_add", test_add)) ||
+      (NULL == CU_add_test(suite, "test_subtract", test_subtract)) ||
+      (NULL == CU_add_test(suite, "test_multiply", test_multiply)) ||
+      (NULL == CU_add_test(suite, "test_divide", test_divide))) {
+    CU_cleanup_registry();
+    return CU_get_error();
+  }
+
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  CU_basic_run_tests();
+
+  CU_cleanup_registry();
+  return CU_get_error();
 }
